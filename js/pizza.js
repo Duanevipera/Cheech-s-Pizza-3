bcc7406fc0cdacc9c9e6e086a53423392d71c186
Tray Northern <tnorthern@yahoo.com>
2019-02-28 20:24:06 -0600

    added the selection boxes for type and quanity, calculateTotal function still progress, also added more life to the carousel as well as added a stylesheet

--- js/pizza.js
+++ js/pizza.js
@@ -1,6 +1,7 @@
-var dropDown = document.querySelector(".adiv");
-let x = 0;
+var numOfPizzas = document.querySelector(".adiv");
+let z = 0;
 let i = 0;
+let types = [['Peperoni', 5.99], ['Sausage', 5.99], ['Cheese', 2.50], ['Meatlover', 8.99], ['Crunchaton', 15.95]];
 console.log(i)
 function displayNums(){
     for(x = 1; x < 11; x++){
@@ -11,6 +12,21 @@ function displayNums(){
     return i;
 }
 
-dropDown.innerHTML += "<select name='amount' id='nums'>" +  displayNums() +    "</select>"
-dropDown.innerHTML += "<h1>firejf9eiofk</h1>"
-console.log(dropDown.innerHTML)
+function displayTypes() {
+  for (x = 0; x < types.length; x++) {
+   z += "<option value=' " + types[x][0] + "'>" + types[x][0] + " $" + types[x][1] + "</option>"
+
+
+  }
+ return z; 
+}
+
+numOfPizzas.innerHTML += "<b id='typeLabel'>Types</b><select name='type' id='type'>" + displayTypes() + "</select>"
+numOfPizzas.innerHTML += "<b id='quanityLabel'>Quantity</b><select name='amount' id='nums'>" +  displayNums() +    "</select>"
+numOfPizzas.innerHTML += "<input class='submit' type='submit' value='Order Now'></input>"
+
+function calculateTotal(){
+  //get price based off of type * quanity
+}
+
+console.log(numOfPizzas)
